955ad2bbdea0baaee0531200e800097f6e2593e2
Vincent Sheffer <vincent.sheffer@bluemvmt.com>
2024-12-01 17:28:05 -0500

    Assert that the JSON was parsed.

--- tests/test_deserialize.py
+++ tests/test_deserialize.py
@@ -3,4 +3,4 @@ from bluemvmt_gsf.models import GsfRecord, deserialize_record
 
 def test_swath_bathymetric_ping(swath_bathymetric_ping_json):
     record: GsfRecord = deserialize_record(swath_bathymetric_ping_json)
-    print(f"record={record.record_type}")
+    assert record is not None
