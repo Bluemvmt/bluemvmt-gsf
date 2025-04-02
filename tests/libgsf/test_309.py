ffb5367d57f0e3580e6f88ec2df91f2b4f3c91f6
Vincent Sheffer <vincent.sheffer@bluemvmt.com>
2025-04-02 22:31:24 +0000

    Added GSF_VERSION environment variable.

--- tests/libgsf/test_309.py
+++ tests/libgsf/test_309.py
@@ -1,5 +1,9 @@
-from bluemvmt_gsf.libgsf import GsfFile, open_gsf
+import os
 
+# This has to come before the import of libgsf after it
+os.environ["GSF_VERSION"] = "03.09"
+
+from bluemvmt_gsf.libgsf import GsfFile, open_gsf
 
 def test_to_json(gsf_file_name):
     gsf_file: GsfFile = open_gsf(gsf_file_name)
